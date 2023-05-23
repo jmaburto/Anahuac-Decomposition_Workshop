6351e5c00324d532f8c25c8031c73f27cae61913
Jose Manuel Aburto <jm.aburto01@gmail.com>
2023-05-22 19:00:00 -0600

    day 2 materials

---
+++ Day 2/Exercise 3/Exercise3_students.R
@@ -0,0 +1,20 @@
+# Setup -------------------------------------------------------------------
+
+library(here)
+wd <- here()
+setwd(wd)
+
+load("Day 2/Exercise 3/SullivanData.RData")
+
+library(tidyverse)
+library(viridis)
+
+# Create prevalence lifetable (just the elements we need) -------------------------
+
+
+# Let's look at the results -----------------------------------------------
+
+
+# Let's compare healhty and total life expectancy -------------------------
+
+# Now we can plot it
